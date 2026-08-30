2ffb55fc29dadb4287c783e88afec83cd719828e
MfaizanA21 <mfaizaan771@gmail.com>
2026-08-30 13:21:25 +0500

    Update portfolio

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,22 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Developer',
+    company: "Komatsu (Pvt.) Ltd.",
+    duration: "(July 2026 - Present)",
+    url: "https://home.kps.komatsu/",
+    logo: "/komatsu.svg"
+  },
+  {
+    id: 2,
     title: 'Software Engineer',
     company: "Futurenostics",
-    duration: "(Jan 2025 - Present)",
+    duration: "(Jan 2025 - July 2026)",
     url: "https://www.futurenostics.com/",
     logo: "/futurenostics.svg"
   },
   {
-    id: 2,
+    id: 3,
     title: "Python Developer",
     company: "Aaxon Solutions",
     duration: "(June 2024 - Oct 2024)",
@@ -16,7 +24,7 @@ export const experiences = [
     logo: "/aaxon.svg"
   },
   {
-    id: 3,
+    id: 4,
     title: "Android Intern",
     company: "Business Analytics",
     duration: "(June 2023 - Aug 2023)",
